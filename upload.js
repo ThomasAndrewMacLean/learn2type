26a3fbc36b8134aba6b612845db53aad1d4d6f4f
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-08-15 11:02:25 +0200

    init

---
+++ upload.js
@@ -0,0 +1,10 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+
+const NeoCities = require('neocities');
+// const api = new NeoCities('learn2type', process.env.NEO_PW);
+
+// api.upload([{ name: 'index.html', path: 'dist/index.html' }], function(resp) {
+//     console.log(resp);
+// });
